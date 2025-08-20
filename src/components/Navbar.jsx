0bdd2bf259797c6625dfee1b811a20e9c6919984
Lucas Moreira <lucas.moreira@CELLAVITABRASIL.local>
2025-08-20 12:51:23 -0300

    atualização no navbar, centralizado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-black bg-opacity-90 backdrop-blur-md text-white shadow-md z-50">
-      <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-4">
+      <div className="max-w-7xl mx-auto flex justify-center items-center px-6 py-4">
         
         
 
