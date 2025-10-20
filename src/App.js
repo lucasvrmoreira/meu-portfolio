59dcc3af3b70cf97be6a610f67c0a7d4e8e2ed07
Lucas Moreira <lucas.moreira@CELLAVITABRASIL.local>
2025-10-20 13:58:07 -0300

    atualizacao curriculo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
+// src/App.js
 import React from "react";
 import Hero from "./components/Hero";
 import Habilidades from "./components/Habilidades";
 import Experiencia from "./components/Experiencia";
 import Projetos from "./components/Projetos";
 import Navbar from "./components/Navbar";
-import Jurandir from "./components/Jurandir/Jurandir";
 
 function App() {
   return (
     <div className="bg-white">
       <Navbar />
-      <Jurandir /> {/* fica fixo, a posição no DOM não importa */}
       <section id="hero"><Hero /></section>
       <section id="habilidades"><Habilidades /></section>
       <section id="experiencia"><Experiencia /></section>
